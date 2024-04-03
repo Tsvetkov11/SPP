478af694afa83de44986c626db90649a61ca531b
Tsvetkov11 <152094006+Tsvetkov11@users.noreply.github.com>
2024-04-03 13:46:54 +0300

    Add files via upload

---
+++ lab spp №2(2).cpp
@@ -0,0 +1,33 @@
+#include <iostream> 
+
+#include <cmath> 
+
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+
+void radiansToDegrees(double radians, double& degrees) {
+
+    degrees = (radians * 180.0) / PI;
+
+}
+
+int main() {
+
+    double angleRadians;
+
+    cout << "Enter the angle in radians: ";
+
+    cin >> angleRadians;
+
+    double angleDegrees;
+
+    radiansToDegrees(angleRadians, angleDegrees);
+
+    cout << "Angle in radians: " << angleRadians << endl;
+
+    cout << "Angle in degrees: " << angleDegrees << endl;
+
+    return 0;
+
+}
